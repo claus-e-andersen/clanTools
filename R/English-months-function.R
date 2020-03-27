7207e74123f62149ae365a21df4a563f3eec6afa
claus.e.andersen <clan@dtu.dk>
2020-03-27 10:42:53 +0100

    again

--- R/English-months-function.R
+++ R/English-months-function.R
@@ -1,20 +1,24 @@
 #' @title  Print names of months in English
 #' @description 
-#' Convert numbers 1 to 12 into "January", "February" etc.
+#' Convert numbers from 1 to 12 into "January", "February" etc.
+#' This version handles vectorized input, and NA's and NULL.
 #' @usage  English.months(1)
 #' @name English.months
 #' @author Claus E. Andersen
-#' @return string
-#' @param i number between 1 and 12.
+#' @return string vector
+#' @param i vector with numbers between 1 and 12.
 #' @export English.months
 English.months <- function(i=1){
-  res <- "Not.a.valid.month"
-  if(is.null(i) || is.na(i)){} else {
-    mvec<-c("January","February","March","April","May","June","July","August","September","October","November","December")
-    i <- as.integer(i)
-    if(i<1|i>12){} else {
-      res <- mvec[i]}
+  mvec<-c("January","February","March","April","May","June","July","August","September","October","November","December")
+  if(is.null(i)) {i <- NA}
+  res <- rep("Not.a.valid.month",length(i)) 
+  
+  ok <- !is.na(i) & (i>=1) & (i<=12)
+  if(sum(ok)>0){
+    j <- 1:length(i)
+    res[j[ok]] <- mvec[i[ok]] 
   }
+  
   res
 }# English.months
 
