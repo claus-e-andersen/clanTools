4b77e7999412da94adf5fd5a9c0b7c8e99f143f6
claus.e.andersen <clan@dtu.dk>
2020-03-27 21:35:55 +0100

    Added and updated get.date.range

--- R/English-months-function.R
+++ R/English-months-function.R
@@ -23,3 +23,43 @@ English.months <- function(i=1){
 }# English.months
 
 
+
+#' @title  Make string with range of dates (eg. for calibration certificates)
+#' @description 
+#' Compute the range of dates and present results using English months, like
+#' "January 01, 2020 -- January 31, 2020". If there is only one date, then
+#' the output will be something like "January 01, 2020 (one day only)".
+#'  
+#' Example:
+#' 
+#' get.date.range(x="31-01-2020")
+#' 
+#' get.date.range(x=c("01-01-2020","01-01-2020"))
+#' 
+#' get.date.range(x=c("01-01-2020","31-01-2020"))
+#' 
+#' @usage  #' get.date.range(x=c("01-01-2020","31-01-2020"))
+#' @name get.date.range
+#' @author Claus E. Andersen
+#' @return string with human readable text.
+#' @param x vector with dates in the format "31-01-2020".
+#' @export get.date.range
+get.date.range <- function(x){
+  xxx <- paste(unique(as.character(x)),sep=" ", collapse=" ")
+  yyy <-  unlist(strsplit(xxx,split=" "))
+  #zzz <- paste(unique(as.character(yyy)),collapse=" ", sep=" ")
+  #x <- zzz
+  x <- yyy
+  x <- unique(x)
+  x <- sort(x)
+  x1 <- first.element(x)
+  x2 <- last.element(x)
+  xx.value1 <- paste(English.months(as.numeric(substring(x1,4,5)))," ",substring(x1,1,2),", ",substring(x1,7,10),sep="")
+  xx.value2 <- paste(English.months(as.numeric(substring(x2,4,5)))," ",substring(x2,1,2),", ",substring(x2,7,10),sep="")
+  xx.value0 <- paste(xx.value1," -- ",xx.value2,"",sep="")
+  if(x1==x2){
+    xx.value0 <- paste(xx.value1," (one day only)",sep="")
+  }
+  xx.value0 
+} # get.date.range
+
